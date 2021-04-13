e9950cdededc4ce1ad0b1ef64dabed95f4f3ed5d
Ahmad Faraz <ahmadfarazjutt3@gmail.com>
2021-04-13 22:31:10 +0500

    Updates

--- sw.js
+++ sw.js
@@ -57,7 +57,7 @@ var __wpo = {
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "4/13/2021, 10:22:37 PM",
+  "version": "4/13/2021, 10:31:09 PM",
   "name": "webpack-offline",
   "pluginVersion": "5.0.6",
   "relativePaths": false
